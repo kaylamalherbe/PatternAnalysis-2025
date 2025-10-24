24b0d9b78d421805aa378d96cefb7708cc9cfd6b
kayla.malherbe <s4747715@student.uq.edu.au>
2025-10-24 12:12:52 +1000

    Attempted updated learning rate and reduce dataset - performance below 0.5 - model extremely slow - valdiation isn't increasing

--- recognition/CovNetX_s4747715/dataset.py
+++ recognition/CovNetX_s4747715/dataset.py
@@ -10,44 +10,6 @@ import numpy as np
 
 IMAGE_DIM = 224
 
-# Load data set
-def data_loader(dir, batch_size=64, split=0.3, seed=3710):
-    # augement data
-
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-
-
-    dataset = datasets.ImageFolder(dir, transform=transform)
-
-    train_size = int(len(dataset) * (1 - split))
-    val_size = len(dataset) - train_size
-    generator = torch.Generator().manual_seed(seed)
-    
-    train_set, val_set = random_split(dataset, [train_size, val_size], generator=generator)
-
-    train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True) 
-    val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True)    
-
-    return train_dataloader, val_dataloader
-
-
-def test_loader(dir, batch_size):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-
-    dataset = datasets.ImageFolder(dir, transform=transform)
-
-    val_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-
-    return val_dataloader
-
 
 def calc_normalization_values(dir):
   # define custom transform function
@@ -56,9 +18,6 @@ def calc_normalization_values(dir):
       transforms.ToTensor()
   ])
   img_tr = datasets.ImageFolder(dir, transform=transform)[0][0]
-  # transform the pIL image to tensor
-  # image
-  # img_tr = transform(img)
 
   # Convert tensor image to numpy array
   img_np = np.array(img_tr)
@@ -98,8 +57,8 @@ def data_loader_aug(dir, batch_size=64, split=0.3, seed=3710, mean=[0.1164], std
 
     dataset = datasets.ImageFolder(dir, transform=transform)
     #random choose half the dataset
-    # dataset, _ = random_split(dataset, [len(dataset)//4, 3*len(dataset)//4])
-    # dataset, _ = random_split(dataset, [len(dataset)//4, 3*len(dataset)//4])
+    dataset, _ = random_split(dataset, [len(dataset)//4, 3*len(dataset)//4])
+    dataset, _ = random_split(dataset, [len(dataset)//4, 3*len(dataset)//4])
 
     train_size = int(len(dataset) * (1 - split))
     val_size = len(dataset) - train_size
--- recognition/CovNetX_s4747715/train.py
+++ recognition/CovNetX_s4747715/train.py
@@ -71,26 +71,26 @@ def train(model, num_epochs, learning_rate, criterion, optimizer, load = False):
                 print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(dl_aug)}], TRAIN Loss: {loss.item():.4f}')
 
         # use validation set to find loss and accuracy
-        val_loss, val_acc = evaluate_model(model, val_dl_aug, criterion, device)
+            val_loss, val_acc = evaluate_model(model, val_dl_aug, criterion, device)
 
-        print(f'Epoch [{epoch+1}/{num_epochs}]')
-        print(f'  -> Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_acc:.4f}')
+            print(f'Epoch [{epoch+1}/{num_epochs}]')
+            print(f'  -> Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_acc:.4f}')
 
-        # Check if this is the best model so far based on Validation Loss
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            best_epoch = epoch + 1
-            patience_counter = 0  # Reset patience if performance improved
+            # Check if this is the best model so far based on Validation Loss
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                best_epoch = epoch + 1
+                patience_counter = 0  # Reset patience if performance improved
 
-            # Save the best model weights found on the validation set
-            torch.save(model.state_dict(), 'best_model_weights.pth')
+                # Save the best model weights found on the validation set
+                torch.save(model.state_dict(), 'best_model_weights.pth')
 
-        else:
-            patience_counter += 1
-            # If the model performance hasn't improved for 'patience_counter' epochs, stop training.
-            if patience_counter >= early_stop_patience:
-                print(f"\n Early stopping triggered after {epoch+1} epochs.")
-                break
+            else:
+                patience_counter += 1
+                # If the model performance hasn't improved for 'patience_counter' epochs, stop training.
+                if patience_counter >= early_stop_patience:
+                    print(f"\n Early stopping triggered after {epoch+1} epochs.")
+                    break
     print("\n> Training Finished.")
         
     return model
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     )
     model = model.to(device)
 
-    learning_rate = 5e-6
+    learning_rate = 1e-4
     criterion = nn.CrossEntropyLoss(label_smoothing=0.1).to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=5e-4)
 
