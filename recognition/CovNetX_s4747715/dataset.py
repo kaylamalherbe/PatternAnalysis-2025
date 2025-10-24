36da25da045f4dcc0c293acb57b4e6914ac3922d
kayla.malherbe <s4747715@student.uq.edu.au>
2025-10-24 11:58:38 +1000

    Updated dataset loading with  augmentation and add normaliziation calc function

--- recognition/CovNetX_s4747715/dataset.py
+++ recognition/CovNetX_s4747715/dataset.py
@@ -5,6 +5,10 @@ from torch.utils.data import random_split
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+
+IMAGE_DIM = 224
 
 # Load data set
 def data_loader(dir, batch_size=64, split=0.3, seed=3710):
@@ -45,6 +49,91 @@ def test_loader(dir, batch_size):
     return val_dataloader
 
 
+def calc_normalization_values(dir):
+  # define custom transform function
+  transform = transforms.Compose([
+      transforms.Grayscale(num_output_channels=1),
+      transforms.ToTensor()
+  ])
+  img_tr = datasets.ImageFolder(dir, transform=transform)[0][0]
+  # transform the pIL image to tensor
+  # image
+  # img_tr = transform(img)
+
+  # Convert tensor image to numpy array
+  img_np = np.array(img_tr)
+
+  mean, std = img_tr.mean([1,2]), img_tr.std([1,2])
+
+  # print mean and std
+  print("mean and std before normalize:")
+  print("Mean of the image:", mean)
+  print("Std of the image:", std)
+
+  return mean, std
+
+
+
+# Load data set
+def data_loader_aug(dir, batch_size=64, split=0.3, seed=3710, mean=[0.1164], std=[0.2307]):
+    # augement data
+
+    # transform = transforms.Compose([
+    #     transforms.Resize((224, 224)),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # ])
+    transform = transforms.Compose([
+      transforms.Resize((IMAGE_DIM, IMAGE_DIM)),
+        transforms.Grayscale(num_output_channels=1),
+        # transforms.RandomHorizontalFlip(p=0.3),
+        # transforms.RandomRotation(degrees=10),
+        # transforms.ColorJitter(brightness=0.1, contrast=0.1),
+        transforms.RandomAffine(degrees=5, translate=(0.02, 0.02), scale=(0.95, 1.05)),
+        # transforms.RandomResizedCrop(size=IMAGE_DIM, scale=(0.9, 1.1)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+        # transforms.RandomErasing(p=0.3, scale=(0.02, 0.05), ratio=(0.3, 3.3)),
+    ])
+
+    dataset = datasets.ImageFolder(dir, transform=transform)
+    #random choose half the dataset
+    # dataset, _ = random_split(dataset, [len(dataset)//4, 3*len(dataset)//4])
+    # dataset, _ = random_split(dataset, [len(dataset)//4, 3*len(dataset)//4])
+
+    train_size = int(len(dataset) * (1 - split))
+    val_size = len(dataset) - train_size
+    generator = torch.Generator().manual_seed(seed)
+
+    train_set, val_set = random_split(dataset, [train_size, val_size], generator=generator)
+    print(len(train_set))
+    print(len(val_set))
+    train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+
+    return train_dataloader, val_dataloader
+
+
+def test_loader_aug(dir, batch_size, mean, std):
+    transform = transforms.Compose([
+      transforms.Resize((IMAGE_DIM, IMAGE_DIM)),
+        transforms.Grayscale(num_output_channels=1),
+        # transforms.RandomHorizontalFlip(p=0.3),
+        # transforms.RandomRotation(degrees=10),
+        # transforms.ColorJitter(brightness=0.1, contrast=0.1),
+        transforms.RandomAffine(degrees=5, translate=(0.02, 0.02), scale=(0.95, 1.05)),
+        # transforms.RandomResizedCrop(size=IMAGE_DIM, scale=(0.9, 1.1)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+        # transforms.RandomErasing(p=0.3, scale=(0.02, 0.05), ratio=(0.3, 3.3)),
+    ])
+
+
+    dataset = datasets.ImageFolder(dir, transform=transform)
+
+    test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    return test_dataloader
 
 
 
--- recognition/CovNetX_s4747715/train.py
+++ recognition/CovNetX_s4747715/train.py
@@ -1,22 +1,27 @@
-import dataset
+from dataset import data_loader_aug, test_loader_aug, calc_normalization_values
 import pickle
+import torch
+import torch
 
-data_location = r"C:\Users\kayla\Documents\Uni\2025 Sem 2\COMP3710\Final Project\ADNI data set for Alzheimer's disease-20251015T011003Z-1-001\AD_NC\train"
-validation_location = r"C:\Users\kayla\Documents\Uni\2025 Sem 2\COMP3710\Final Project\ADNI data set for Alzheimer's disease-20251015T011003Z-1-001\AD_NC\test"
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning CUDA not Found. Using CPU")
+
+
+data_location = r"FILE-PATH\ADNI data set for Alzheimer's disease-20251015T011003Z-1-001\AD_NC\train"
+test_location = r"FILE-PATH\ADNI data set for Alzheimer's disease-20251015T011003Z-1-001\AD_NC\test"
 
 if __name__ == "__main__":
-    dl, val_dl = dataset.data_loader(data_location, batch_size=32)
-    test_dl = dataset.test_loader(validation_location, batch_size=32)
 
-    # print size of data_loaders
-    print(len(dl))
-    print(len(val_dl))
-    print(len(test_dl))
+    mean, std = calc_normalization_values(data_location)
+    dl_aug, val_dl_aug = data_loader_aug(data_location, batch_size=64, mean=mean, std=std)
+    test_dl_aug = test_loader_aug(test_location, batch_size=64, mean=mean, std=std)
 
     # save to pickle
-    with open('train_loader.pkl', 'wb') as f:
-        pickle.dump(data_loader, f)
-    with open('val_loader.pkl', 'wb') as f:
-        pickle.dump(val_dl, f)
-    with open('test_loader.pkl', 'wb') as f:
-        pickle.dump(test_dl, f)
+    with open('train_loader_aug.pkl', 'wb') as f:
+        pickle.dump(dl_aug, f)
+    with open('val_loader_aug.pkl', 'wb') as f:
+        pickle.dump(val_dl_aug, f)
+    with open('test_loader_aug.pkl', 'wb') as f:
+        pickle.dump(test_dl_aug, f)
